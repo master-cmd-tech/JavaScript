3d48092e781493014ee82a39cdf0e813029a3df9
master-cmd-tech <abdrahmanov01122006@gmail.com>
2026-01-15 16:44:08 +0500

    commit

---
+++ lesson1/index.js
@@ -0,0 +1,7 @@
+console.log(`Hello`);
+console.log(`I like pizza`);
+
+// window.alert(`This is an alert!`);
+/* This is a comment*/
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like pizza!";
