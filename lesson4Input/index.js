e89d5d0a1159abc3043bef387ab0012c0071ecff
master-cmd-tech <abdrahmanov01122006@gmail.com>
2026-01-19 15:10:58 +0500

    commit

--- lesson4Input/index.js
+++ lesson4Input/index.js
@@ -2,4 +2,15 @@
 
 // 1. EASY WAY = window prompt
 // 2. PROFESSIONAL WAY = HTML textbox
- 
+
+//  let username;
+//  username = window.prompt("What is your name?");
+//  console.log(username);
+
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myH1").textContent = `Hello ${username}`;
+}
